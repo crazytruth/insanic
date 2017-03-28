faf4e9d7be272c090da779ab40ac3c1216ff6dfe
david <david@mymusictaste.com>
2017-03-28 20:24:32 +0900

    initial commit insanic

--- insanic/__init__.py
+++ insanic/__init__.py
@@ -1,7 +1,16 @@
 from sanic import Sanic
 from sanic_useragent import SanicUserAgent
 
-import config
+try:
+    import config
+except ImportError:
+    config = {}
+
+try:
+    import instance
+except ImportError:
+    instance = {}
+
 from .handlers import ErrorHandler
 from .utils import attach_middleware
 
@@ -11,3 +20,4 @@ SanicUserAgent.init_app(app)
 attach_middleware(app)
 
 app.config.from_object(config)
+app.config.from_object(instance)
