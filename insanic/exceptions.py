69022636ff6ec400486c35db088b9e2dc91f93dd
david <david@mymusictaste.com>
2017-04-05 16:21:04 +0900

    updated with token verification api

--- insanic/exceptions.py
+++ insanic/exceptions.py
@@ -34,6 +34,10 @@ class BadRequest(APIException):
 class InvalidUsage(APIException):
     status_code = status.HTTP_400_BAD_REQUEST
 
+class ValidationError(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+
+
 class NotFound(APIException):
     status_code = status.HTTP_404_NOT_FOUND
     default_detail = 'Not found.'
@@ -47,7 +51,6 @@ class AuthenticationFailed(APIException):
 class NotAuthenticated(APIException):
     status_code = status.HTTP_401_UNAUTHORIZED
     default_detail = 'Authentication credentials were not provided.'
-
 
 
 class PermissionDenied(APIException):
