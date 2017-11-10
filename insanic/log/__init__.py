ddedf01cb9ad18c1dd77781c8c4fc056554efd66
david <david@mymusictaste.com>
2017-11-10 15:23:04 +0900

    show exceptions in elasticsearch

--- insanic/log/__init__.py
+++ insanic/log/__init__.py
@@ -18,7 +18,7 @@ LOGGING['formatters']['json'] = {
     'datefmt': '%Y-%m-%dT%H:%M:%S.%%(msecs)d%z'
 }
 
-if os.environ.get('MMT_ENV') != "local" or True:
+if os.environ.get('MMT_ENV') != "local":
 
     LOGGING['handlers']['accessStream']['formatter'] = 'json'
     LOGGING['handlers']['accessStream']['stream'] = sys.stdout
