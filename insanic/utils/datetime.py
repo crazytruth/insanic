77562d0d1b381a6f5d893d33786af559caa76122
david <david@mymusictaste.com>
2018-02-02 14:01:48 +0900

    <bug> (insanic) datetime util function where return local times

--- insanic/utils/datetime.py
+++ insanic/utils/datetime.py
@@ -1,23 +1,27 @@
 from datetime import datetime, timezone
-import time
+
+
+def get_utc_timestamp():
+    return datetime.utcnow().timestamp()
 
 
 def utc_to_datetime(timestamp=None, units=None):
+
     if timestamp is None and units is None:
-        timestamp = time.time()
-        units = "ms"
+        timestamp = get_utc_timestamp()
+        units = "s"
     elif timestamp is not None and units is None:
         raise ValueError("You must pass in the timestamp units. [s/ms]")
     elif timestamp is None and units is not None:
         if units == "s":
-            timestamp = int(time.time())
+            timestamp = int(get_utc_timestamp())
         elif units == "ms":
-            timestamp = int(time.time() * 1000)
+            timestamp = int(get_utc_timestamp() * 1000)
         else:
             raise ValueError(f"{units} is an invalid units input. Must be either [s/ms].")
-    else:
-        if units == "ms":
-            timestamp = timestamp / 1000
+
+    if units == "ms":
+        timestamp = timestamp / 1000
     return datetime.fromtimestamp(timestamp).replace(tzinfo=timezone.utc)
 
 
