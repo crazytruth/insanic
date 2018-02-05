b97eeb806231d059739663fdad8de93056c7e20f
david <david@mymusictaste.com>
2018-02-05 19:20:21 +0900

    Back to development: 0.0.171

--- commands/releaser.py
+++ commands/releaser.py
@@ -1,3 +1,17 @@
+import requests
+
+def _slack_developers(new_version):
+    params = {}
+    params['channel'] = '#dev-project-msa'
+    params['username'] = "INSANIC RELEASE!"
+    params['text'] = f'New version [{new_version}] has been released. `pip install -U insanic` to update.'
+    params['icon_emoji'] = ":sanic:"
+
+    slack_webhook_url = 'https://hooks.slack.com/services/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'
+
+    # f = urllib.urlopen(slack_webhook_url, params)
+    r = requests.post(slack_webhook_url, data=json.dumps(params))
+
 def middle(data):
-    print(data)
+    _slack_developers(data[''])
     return
