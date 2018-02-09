94e5571528fe446bd86b15b23e248f9e65ef9abf
david <david@mymusictaste.com>
2018-02-09 12:16:57 +0900

    <chore> (insanic) refactor error codes for allocated error namespace

--- insanic/errors.py
+++ insanic/errors.py
@@ -3,26 +3,25 @@ from enum import Enum
 
 class GlobalErrorCodes(Enum):
     # authentication errors
-    authentication_credentials_missing = 90001
-    invalid_authorization_header = 90002
-    permission_denied = 90010
+    authentication_credentials_missing = 999001
+    invalid_authorization_header = 999002
+    permission_denied = 999010
 
     # token related errors
-    signature_expired = 90101
-    signature_not_decodable = 90102
-    invalid_payload = 90103
-    invalid_token = 90104
-    invalid_signature = 90105
+    signature_expired = 999101
+    signature_not_decodable = 999102
+    invalid_payload = 999103
+    invalid_token = 999104
+    invalid_signature = 999105
 
     # database related
-    redis_unable_to_process = 90201
+    redis_unable_to_process = 999201
 
     # general errors
-    invalid_usage = 90301
+    invalid_usage = 999301
 
     # server related errors
-    server_signature_error = 99001
-    service_unavailable = 99503
-    service_timeout = 99504
-    unknown_error = 99999
-
+    server_signature_error = 999001
+    service_unavailable = 999503
+    service_timeout = 999504
+    unknown_error = 999999
