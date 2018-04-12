13e0760b50250d5559322dbbb8079a0156440ecd
david <david@mymusictaste.com>
2018-04-12 21:59:52 +0900

    <feat> (insanic) public facing decorator

--- insanic/scopes.py
+++ insanic/scopes.py
@@ -1,12 +1,16 @@
-from functools import wraps
+import os
 
 
-def public_facing(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        return func(*args, **kwargs)
+class public_facing:
+    def __init__(self, f):
+        self.func = f
+        self.scope = "public"
+        self.__name__ = f.__name__
+        self.__doc__ = f.__doc__
+        self.__module__ = f.__module__
 
-    return func
+    def __call__(self, *args, **kwargs):
+        return self.func(*args, **kwargs)
 
 
 def _is_docker():
@@ -23,3 +27,13 @@ def _is_docker():
 
 
 is_docker = _is_docker()
+
+
+def get_machine_id():
+    if is_docker:
+        machine_id = os.environ.get('HOSTNAME')
+    else:
+        import socket
+        ip = socket.gethostbyname(socket.gethostname())
+        machine_id = '{:02X}{:02X}{:02X}{:02X}'.format(*map(int, ip.split('.')))
+    return machine_id
