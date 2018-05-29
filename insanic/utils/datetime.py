daad6687326ddf7ebaaa1ea7c7060000a0323e72
david <david@mymusictaste.com>
2018-05-29 20:42:55 +0900

    <feat> (insanic) added several datetime util functions like iso_to_utc Closes #94

--- insanic/utils/datetime.py
+++ insanic/utils/datetime.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timezone
+from dateutil import parser
 
 VALID_UNITS = {'s': 1, 'ms': 1000}
 
@@ -7,14 +8,18 @@ def get_utc_timestamp():
     return datetime.now(tz=timezone.utc).timestamp()
 
 
-def utc_to_datetime(timestamp=None, units_hint=None):
-    if timestamp is None and units_hint is None:
+def get_utc_datetime():
+    return datetime.fromtimestamp(get_utc_timestamp())
+
+
+def utc_to_datetime(timestamp=None, units=None):
+    if timestamp is None and units is None:
         timestamp = get_utc_timestamp()
-    elif timestamp is not None and units_hint is not None:
-        if units_hint in VALID_UNITS.keys():
-            timestamp = timestamp / VALID_UNITS[units_hint]
+    elif timestamp is not None and units is not None:
+        if units in VALID_UNITS.keys():
+            timestamp = timestamp / VALID_UNITS[units]
         else:
-            raise ValueError(f"{units_hint} is an invalid `units_hint` input. Must "
+            raise ValueError(f"{units} is an invalid `units_hint` input. Must "
                              f"be either [{'/'.join(VALID_UNITS.keys())}].")
     else:
         raise ValueError("If passing arguments both, timestamp and `units_hint`, are required.")
@@ -22,5 +27,16 @@ def utc_to_datetime(timestamp=None, units_hint=None):
     return datetime.fromtimestamp(timestamp, tz=timezone.utc)
 
 
+def utc_seconds_to_datetime(timestamp):
+    return utc_to_datetime(timestamp=timestamp, units='s')
+
+
+def utc_milliseconds_to_datetime(timestamp):
+    return utc_to_datetime(timestamp=timestamp, units='ms')
+
 def utc_to_iso(timestamp=None, units_hint=None):
-    return utc_to_datetime(timestamp, units_hint).isoformat(timespec='milliseconds')
+    return utc_to_datetime(timestamp, units_hint).isoformat(timespec='microseconds')
+
+
+def iso_to_utc(datetime_string):
+    return parser.parse(datetime_string).astimezone(timezone.utc)
