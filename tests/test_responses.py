93b8f3ae8073c17a169dbfe62174dcd4888e5207
david <david@mymusictaste.com>
2018-07-18 12:38:51 +0900

    <fix> (insanic) test fixes

--- tests/test_responses.py
+++ tests/test_responses.py
@@ -8,7 +8,6 @@ from insanic.responses import json_response
         {"Content-Length": 4}
 ))
 def test_malformed_204_response_has_no_content_length(headers):
-    # flask-restful can generate a malformed response when doing `return '', 204`
 
     response = json_response({}, status=status.HTTP_204_NO_CONTENT, headers=headers)
     assert response.status == status.HTTP_204_NO_CONTENT
@@ -16,6 +15,6 @@ def test_malformed_204_response_has_no_content_length(headers):
 
     http_response = response.output().split(b'\r\n')
 
-    assert bytes(status.HTTP_204_NO_CONTENT) in http_response[0]
+    assert b"204" in http_response[0]
     assert b"No Content" in http_response[0]
     assert b"Content-Type" not in http_response[2]
