8412d70dff9d282f6473512f28ebe53950352282
crazytruth <david@mymusictate.com>
2020-09-08 18:09:52 +0900

    <fix>: fixes bug with service connections not closing

--- insanic/listeners.py
+++ insanic/listeners.py
@@ -2,7 +2,7 @@ import aiotask_context
 import asyncio
 
 from insanic.connections import _connections
-from insanic.services import Service
+from insanic.services.registry import registry
 
 
 def before_server_start_verify_plugins(app, loop, **kwargs):
@@ -13,16 +13,23 @@ def before_server_start_set_task_factory(app, loop, **kwargs):
     loop.set_task_factory(aiotask_context.chainmap_task_factory)
 
 
+async def after_server_start_connect_database(app, loop=None, **kwargs):
+    _connections.loop = loop
+
+
 async def after_server_stop_clean_up(app, loop, **kwargs):
+    """
+    Clean up all connections and close service client connections.
+    :param app:
+    :param loop:
+    :param kwargs:
+    :return:
+    """
     close_tasks = _connections.close_all()
     await close_tasks
 
-    if Service._client is not None:
-        await Service._client.aclose()
-        await asyncio.sleep(0)
-
+    close_client_tasks = [
+        service.close_client() for service in registry.values()
+    ]
+    await asyncio.gather(*close_client_tasks)
     await asyncio.sleep(0)
-
-
-async def after_server_start_connect_database(app, loop=None, **kwargs):
-    _connections.loop = loop
