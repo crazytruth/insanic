5efa0cf43152d0866e0c9e8d4f31dc00243d2b9c
crazytruth <david@mymusictate.com>
2020-09-29 15:58:42 +0900

    <chore>: update setup for release

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open("insanic/__init__.py", encoding="utf8") as f:
 setup(
     name="insanic",
     version=version,
-    description="API framework for sanic",
+    description="An API framework that extends sanic with a focus on microservices.",
     long_description=(read("README.rst") + "\n\n" + read("CHANGELOG.rst")),
     classifiers=[
         "Intended Audience :: Developers",
@@ -33,11 +33,11 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Topic :: Software Development :: Libraries :: Application Frameworks",
-        "Framework :: Sanic"
+        "Framework :: Sanic",
     ],
-    keywords="api framework sanic async asyncio microservice msa",
-    url="http://github.com/crazytruth/insanic",
-    author="crazytruth",
+    keywords="api framework sanic async asyncio microservice msa python python3",
+    url="https://github.com/crazytruth/insanic",
+    author="Kwang Jin Kim",
     author_email="kwangjinkim@gmail.com",
     license="MIT",
     packages=find_packages(
@@ -51,9 +51,15 @@ setup(
         "aiotask_context",
         "python-dateutil",
         "prometheus-client==0.5.0",
-        "psutil==5.4.8",
+        "psutil",
     ],
     test_suite="tests",
     include_package_data=True,
     zip_safe=False,
+    project_urls={
+        "Documentation": "https://insanic.readthedocs.io/en/latest/",
+        "Source": "https://github.com/crazytruth/insanic",
+        "Tracker": "https://github.com/crazytruth/insanic/issues",
+    },
+    python_requires=">=3.6",
 )
